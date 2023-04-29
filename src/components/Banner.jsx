2cab98c1125b45f2215a4046b6b6f02188a5a967
HassanShanJava <hassanshan675@gmail.com>
2023-04-29 16:17:36 +0500

    made banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='bg-purple-800 p-2 text-slate-200 w-full  '>
+        <p className='text-center animate-pulse'>Exciting changes are coming to our website! Please pardon our appearance while we work behind the scenes.</p>
+    </div>
+  )
+}
+
+export default Banner
